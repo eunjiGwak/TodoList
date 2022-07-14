6e18c0db5a2ad245179dc45052e74fbcb93ef32b
곽은지_PC <_PC@GWAK>
2022-07-14 19:08:31 +0900

    3차 - 컨트롤러 생성

---
+++ TodoList/src/main/java/com/todo/make/TodoController.java
@@ -0,0 +1,17 @@
+package com.todo.make;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todolist")
+public class TodoController {
+
+	@GetMapping("")
+	public String index() {
+		
+		return "index";
+	}
+	
+}
